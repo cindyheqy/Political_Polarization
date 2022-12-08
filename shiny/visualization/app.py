c8a72ae8f091c5cbff4ce71fcb020b29a0080750
cindyheqy <cindy.heqy@gmail.com>
2022-12-07 23:36:47 -0600

    shiny web page

--- shiny/visualization/app.py
+++ shiny/visualization/app.py
@@ -9,14 +9,17 @@ from matplotlib import colors
 
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello Shiny!"),
+    ui.h1("Political Polarization", align="center"),
     ui.input_date_range(
         "daterange1", "Date range:", start="2015-01-01", end="2018-12-31"
     ),
     ui.output_plot('show_rollcall_plot'), 
     ui.input_radio_buttons(id="dataset",
-                            label="Choose a variable",
+                            label="Choose a dataset",
                             choices=["congressional speech", "public opinion"]),
+    ui.input_switch(id="line", 
+                    label="Draw line", 
+                    value="False"),
     ui.output_plot('show_two_plot')
 )
 
@@ -38,11 +41,9 @@ def server(input, output, session):
         df = df.loc[polarity_change['Date']<=pd.to_datetime(input.daterange1()[1])]
         fig, ax = plt.subplots(figsize = (15, 10))
         cmap = colors.ListedColormap(['red', 'blue'])
-        # ax.scatter('Date', 'polarity', data=polarity_change.loc[polarity_change['party']==1], s=1, c='party', cmap=cmap)
         ax.scatter('Date', 'polarity', data=df, s=1, c='party', cmap=cmap)
-        #plt.xticks(np.arange())
         plt.axhline(y = 0, color = 'black', linestyle = '-', linewidth=0.3)
-        # plt.show()
+        ax.set_title("Polarity change in congressional voting records")
         return ax
 
         
@@ -54,15 +55,24 @@ def server(input, output, session):
             f, ax = plt.subplots()
             f.set_figwidth(20)
             f.set_figheight(10)
-            ax.scatter('Time', 'Polarity', data=speech, marker='.')
+            if input.line(): 
+                ax.plot('Time', 'Polarity', data=speech, marker='.')
+            else: 
+                ax.scatter('Time', 'Polarity', data=speech, marker='.')
             plt.legend()
+            ax.set_title("Polarity change in congressional speech (2015-2018)")
             return ax
         else: 
             f, ax = plt.subplots()
             f.set_figwidth(20)
             f.set_figheight(10)
-            ax.scatter('Time', 'Polarity', data=public_opinion, marker='.')
+            if input.line(): 
+                ax.plot('Time', 'Polarity', data=public_opinion, marker='.')
+            else: 
+                ax.scatter('Time', 'Polarity', data=public_opinion, marker='.')
             plt.legend()
+            ax.set_title("Polarity change in public opinion (2015-2018)")
+
             return ax
 
 app = App(app_ui, server)
