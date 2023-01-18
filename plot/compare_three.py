34a69d99a627a064adb1b8f3a85ef9e2d1873099
cindyheqy <cindy.heqy@gmail.com>
2023-01-17 20:31:15 -0600

    generate three images

--- plot/compare_three.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-path = '/Users/qingyi/Documents/uchicago/courses/data_programming_for_public_policy_2/Political_Polarization/tables'
-public_opinion = pd.read_csv(os.path.join(path, 'public_opinion.csv'))
-speech = pd.read_csv(os.path.join(path, 'speech.csv'))
-# roll_call = pd.read_csv(os.path.join(path, 'roll_call.csv'))
